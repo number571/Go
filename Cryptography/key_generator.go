e64504f108922ce408c7e047c65a589818a2adf7
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-09 15:59:25 +0700

    Create key_generator.go

---
+++ Cryptography/key_generator.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"crypto/rand"
+)
+
+func main() {
+	fmt.Println(string(generate(30)))
+}
+
+func generate(max int16) []byte {
+	var slice []byte = make([]byte, max)
+	_, err := rand.Read(slice)
+	if err != nil { return nil }
+    for max = max - 1; max >= 0; max -- {
+        slice[max] = slice[max] % 95 + 33
+    }
+	return slice
+}
