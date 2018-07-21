4a71cf68b084053bf9bd27ee908bd519633ebbdd
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-21 22:30:08 +0700

    Update key_generator.go

--- Cryptography/key_generator.go
+++ Cryptography/key_generator.go
@@ -155,7 +155,7 @@ func generate_key (max int32) []byte {
     _, err := rand.Read(slice)
     if err != nil { return nil }
     for max = max-1; max >= 0; max-- {
-        slice[max] = slice[max] % 95 + 33
+        slice[max] = slice[max] % 94 + 33
     }
     return slice
 }
