cf6022b2774bfb66f916673cfd5ebc273d4255ab
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-24 00:20:32 +0700

    Create input_string.go

---
+++ Templates/input_string.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "bufio"
+    "strings"
+)
+
+func main() {
+    var message string = input_string("> ")
+    fmt.Println(message)
+}
+
+func input_string (text string) string {
+
+    var (
+        reader *bufio.Reader
+        command string
+    )
+
+    fmt.Print(text)
+
+    reader = bufio.NewReader(os.Stdin)
+    command, _ = reader.ReadString('\n')
+
+    return strings.Replace(command, "\n", "", -1)
+}
