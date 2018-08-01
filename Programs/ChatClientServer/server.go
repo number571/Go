f5139ebb8cb00c0641bf05a22e8fd09b4f5cb07e
[#571] <31502293+Number571@users.noreply.github.com>
2018-08-02 03:03:27 +0700

    Create server.go

---
+++ Programs/ChatClientServer/server.go
@@ -0,0 +1,63 @@
+package main 
+
+import (
+    "time"
+    "net"
+    "fmt"
+    "os"
+)
+
+const (
+    PROTOCOL = "tcp"
+    PORT = ":8080"
+    BUFF = 1024
+    QUAN = 5
+)
+
+var (
+    connect [QUAN]net.Conn
+    user uint8 = 0
+)
+
+func main () {
+    listen, err := net.Listen(PROTOCOL, PORT)
+    check_error(err)
+    defer listen.Close()
+    
+    fmt.Println("Server is listening...")
+
+    for {
+        time.Sleep(time.Millisecond * 500)
+        if user < QUAN {
+            connect[user], err = listen.Accept()
+            if err != nil { break }
+            go connection(connect[user])
+        }
+    }
+
+    fmt.Scanln()
+}
+
+func connection (conn net.Conn) {
+    user++
+    var buffer []byte = make([]byte, BUFF)
+    defer conn.Close()
+    for {
+        length, err := conn.Read(buffer)
+        if err != nil { break }
+        result := string(buffer[:length])
+        for i := uint8(0); i < user; i++ {
+            if connect[i] != conn {
+                connect[i].Write([]byte(result))
+            }
+        }
+    }
+    user--
+}
+
+func check_error (err error) {
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+}
