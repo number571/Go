ed39427a894870a39c33f4d68d1b10a90464d393
#571 <31502293+number571@users.noreply.github.com>
2023-06-02 21:38:59 +0700

    Add files via upload

--- docker/app/main.go
+++ docker/app/main.go
@@ -10,11 +10,11 @@ import (
 func main() {
 	log.Printf("Service is listening... [%s]\n", os.Args[1])
 	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
-		// res, err := os.ReadFile("/app/mounted/test.txt")
-		// if err != nil {
-
-		// }
-		fmt.Fprint(w, "hello, world!")
+		res, err := os.ReadFile("/mounted/test.txt")
+		if err != nil {
+			fmt.Fprintf(w, "error: %s", err.Error())
+		}
+		fmt.Fprint(w, string(res))
 	})
 	log.Println(http.ListenAndServe(os.Args[1], nil))
 }
